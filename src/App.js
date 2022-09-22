d9779bf419dfb9a5b97ccd1f7768cf9aa0c1e428
sebetc4 <seb-etc@hotmail.fr>
2022-09-22 15:10:03 +0200

    modif text

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenue sur le futur portfolio de Sébastien ETCHETO et Croquette!
+          Bienvenue sur le futur portfolio de Sébastien ETCHETO
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
