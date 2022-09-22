5505935261cfd2b0d10881cd5bd9b94c1e3eaf02
sebetc4 <seb-etc@hotmail.fr>
2022-09-22 14:54:51 +0200

    modif title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenue sur le futur portfolio de Sébastien ETCHETO
         </p>
         <a
           className="App-link"
