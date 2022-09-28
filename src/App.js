e2c1eb25f8542a4f324557000db437a3013c5028
sebetc4 <seb-etc@hotmail.fr>
2022-09-28 21:15:08 +0200

    modif text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.NODE_ENV);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bienvenue sur le futur portfolio de Sébastien ETCHETO </h1>
+        <h2>Développeur web full stack</h2>
         <p>
-          Bienvenue sur le futur portfolio de Sébastien ETCHETO
+          Ce site est en construction, pour patienter vous pouvez visionner la
+          présentation de l'un de mes projets:
         </p>
-        <p>Ce site est en construction, si vous souhaitez me contacter:</p>
-        <a href='mailto:sebastien.etcheto@gmail.com'>sebastien.etcheto@gmail.com</a>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/8wfDbHOJofE"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>{" "}
+        <p>Sinon pour me contacter voici mon mail:</p>
+        <a href="mailto:sebastien.etcheto@gmail.com">
+          sebastien.etcheto@gmail.com
+        </a>
       </header>
     </div>
   );
